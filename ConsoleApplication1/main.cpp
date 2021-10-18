7629abc757db0448e3ce709669d83efb16377ab6
wangbin <wangbin@inovance.com>
2021-10-18 14:27:19 +0800

    exe访问dll类

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -2,8 +2,21 @@
 //
 
 #include <iostream>
+#include <memory>
+
+#include "CInoUAClient.h"
+#include "CInoUAClientMgr.h"
+#include "CInoUAClientProxy.h"
+
+using namespace std;
 
 int main()
 {
+    shared_ptr<CInoUAClient> spClient = make_shared<CInoUAClient>();
+
+    shared_ptr<CInoUAClientMgr> spClientMgr = make_shared<CInoUAClientMgr>();
+
+    shared_ptr<CInoUAClientProxy> spClientProxy = make_shared<CInoUAClientProxy>();
+
     std::cout << "Hello World!\n";
 }
